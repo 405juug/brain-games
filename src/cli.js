c47f20617308718094874bb549b1bc387a10d3c9
niceice1311 <niceice1311@gmail.com>
2025-04-28 13:46:47 +0300

    first

---
+++ src/cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync'
+
+export default (brainGames) => {
+    console.log('Welcome to the Brain Games!')
+    const name = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${name}!`)
+}
+
+
+
