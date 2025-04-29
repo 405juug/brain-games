c663603cef0ff48960861fc2554535787cbff72b
405juug <scaryyow@mail.ru>
2025-04-29 13:36:40 +0300

    first

--- src/cli.js
+++ src/cli.js
@@ -6,4 +6,25 @@ export default function brainGames() {
     console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`)
+
+    function isEven(num){
+        return num % 2 === 0 
+    }
+
+    function evenGame(){
+        console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
+        let count = 0;
+
+        while(count < 3){
+            const randomNumber = Math.floor(Math.random() * 100);
+            console.log(`Question: ${randomNumber}`);
+            const answer = readlineSync.question("Your answer: (yes/no)");
+
+            let correct;
+            if(isEven(randomNumber)){
+                
+            }
+        }
+    }
 }
